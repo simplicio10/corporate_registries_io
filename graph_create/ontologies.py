0d2373e0f0688ce09a4827908f9e267b8e72c3d2
simplicio10 <claypas@gmail.com>
2024-10-23 06:11:28 -0500

    Refactored config class as Pydantic fields

--- graph_create/ontologies.py
+++ graph_create/ontologies.py
@@ -1,21 +1,36 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from pathlib import Path
+import logging
 from cachetools import cached, LRUCache
 from rdflib import URIRef, Namespace
 
-
+logger = logging.getLogger(__name__)
 
 class OntologyConfig(BaseModel):
-    '''Config file to store ontology IRIs and namespaces'''
-    soli_path: str = '../soli_ontology/soli.owl' #Forked version of ontology
-    il_prefix: str = "IL:"
-    vcard_prefix: str = "http://www.w3.org/2006/vcard/ns"
-    cache_dir: str = '.ontology_cache' #Directory for caching ontology info
+    '''Stores IRIs and namespaces'''
+    il_prefix: str = Field(
+        default='IL:',
+        description='Prefix for Illinois entities'
+        )
+    vcard_prefix: str = Field(
+        default='http://www.w3.org/2006/vcard/ns',
+        description='Prefix for IRIs in VCard ontology'
+        )
+    soli_path: str = Field(
+        default='../soli_ontology/soli.owl',
+        description='File path for forked version of ontology'
+        )
+    cache_dir: str = Field(
+        default='.ontology_cache'
+        description='Directory for caching ontology info'
+        ) 
 
 class OntologyError(Exception): #Refactor
     '''Base exception for ontology-related errors'''
-    pass
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
 
 class InvalidIRIError(OntologyError): #Refactor
     pass
@@ -42,22 +57,23 @@ class Ontologies(BaseModel):
         self.cache_dir = Path(self.config.cache_dir)
         self.cache_dir.mkdir(exist_ok=True)
 
-    
+        self.load_and_validate_ontology()
 
-        
-    
-    IL_LLCS = Namespace("IL:") #Custom Namespace ID
-    VCARD = Namespace("http://www.w3.org/2006/vcard/ns") #VCard namespace for generic person and org concepts
-    SOLI_IRI_PREFIX = "https://soli.openlegalstandard.org/"
+        #Initialize namespaces
+        self.IL_LLCS = Namespace(self.config.il_prefix)
+        self.VCARD = Namespace(self.config.vcard_prefix)
+        self.SOLI = f"file://{self.soli_owl_path.absolute()}"
+
+    def load_and_validate_ontology(self) -> None:
+        pass
 
     @classmethod
     def get_entity_id_iri(cls):
-        return URIRef(f"{cls.SOLI_IRI_PREFIX}R955FuA1bPwrZWwF8Nc3v8A")
+        return URIRef(f"{cls.SOLI}R955FuA1bPwrZWwF8Nc3v8A")
 
     @classmethod
     def get_llc_iri(cls):
-        return URIRef(f"{cls.SOLI_IRI_PREFIX}R98E2kVwPFwTnP6oxYUt9HR")
+        return URIRef(f"{cls.SOLI}R98E2kVwPFwTnP6oxYUt9HR")
 
 if __name__ == "__main__":
-    print(Ontologies.get_entity_id_iri())
-    print(Ontologies.get_llc_iri())
+    onto_test = Ontologies()
